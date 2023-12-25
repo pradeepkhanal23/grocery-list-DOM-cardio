ca6a73cc45bbb170b23076d5639c819490f90872
Pradeep Khanal <pradeepkhanal642@gmail.com>
2023-12-25 11:15:27 +1100

    adding, deleting and clearing fucntions cpmplete with refining of fucntions and eventlisteners

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 //-----------------------------DOM Elements-----------------------------------------
 const submitBtn = document.querySelector(".btn");
+const clearBtn = document.querySelector(".btn-clear");
 const itemInput = document.querySelector(".form-input");
 const filterInput = document.querySelector(".form-input-filter");
 const form = document.querySelector("#item-form");
-const itemList = document.querySelector("ul");
-const container = document.querySelector(".container");
+const itemList = document.querySelector("#item-list");
 
 //----------------Create Element Function-----------------------------------------------------
 function createElement(elementType, className = "") {
@@ -13,13 +13,8 @@ function createElement(elementType, className = "") {
   return element;
 }
 
-function appendToUI(li) {
-  itemList.innerHTML = "";
-  itemList.appendChild(li);
-}
-
-//----------------Create List Item(li) Function and append to UL-----------------------------------------------------
-function createLi(LiTextContent) {
+//----------------Create List Item Element(li) with its associated classes and buttons inside it and add it to the unordered list----------------------------------------------------------------------------------
+function createLiElement(LiTextContent) {
   const li = createElement("li");
   const btnContainer = createElement("div", "btn-container");
   const EditButton = createElement("button", "edit-item btn-link text-green");
@@ -27,94 +22,79 @@ function createLi(LiTextContent) {
   const removeIcon = createElement("i", "fa-solid fa-xmark");
   const editIcon = createElement("i", "fa-solid fa-pen-to-square");
   const text = document.createTextNode(LiTextContent);
-
   li.appendChild(text);
   li.appendChild(btnContainer);
   btnContainer.appendChild(removeButton);
   btnContainer.appendChild(EditButton);
   removeButton.appendChild(removeIcon);
   EditButton.appendChild(editIcon);
-
-  addToUL(li);
-}
-
-function addToUL(li) {
   itemList.appendChild(li);
 }
 
-/* Function to handle all the UI related Operation of inserting or deleting elements based on certain actions  */
-function updateUI() {
-  //adding clear all button after a list item is added to the list
-  if (!document.querySelector(".btn-clear")) {
-    const clearBtn = createElement("button", "btn-clear");
-    clearBtn.appendChild(document.createTextNode("Clear All"));
-    itemList.insertAdjacentElement("afterend", clearBtn);
-    clearBtn.addEventListener("click", clearList);
-  }
-
-  /* if after deleting , the last li is deleted, it checks the child element count and remove the clear all button if there is no any li left*/
-  if (itemList.childElementCount < 1) {
-    document.querySelector(".btn-clear").remove();
-  }
-}
-
-//----------------Handle Click Function-----------------------------------------------------
-function handleClick(e) {
+//----------------Delete Function-----------------------------------------------------
+function removeItem(e) {
   e.preventDefault();
 
   //check for the target button and delete
   if (e.target.tagName === "I" && e.target.classList.contains("fa-xmark")) {
-    e.target.parentElement.parentElement.parentElement.remove();
+    if (confirm("Are you sure??"))
+      e.target.parentElement.parentElement.parentElement.remove();
   }
 
+  //check again for UI change as we made a change in the list(i.e removed a data)
   updateUI();
 }
 
-//----------------Handle Filter Function-----------------------------------------------------
-function handleFilter(e) {
-  if (document.querySelectorAll("li").length !== 0) {
-    const items = Array.from(document.querySelectorAll("li"));
-    const targetLi = items
-      .map((item) => {
-        return item;
-      })
-      .filter((item) => {
-        if (item.textContent.includes(e.target.value)) {
-          return item;
-        }
-      })
-      .map((item) => {
-        appendToUI(item);
-      });
-  }
-}
-
-//-------------------Clear List Function---------------------------------------------------
-function clearList(e) {
+//-------------------Clear Items Function---------------------------------------------------
+function clearItems(e) {
   e.preventDefault();
 
-  itemList.innerHTML = "";
-  e.target.remove();
+  while (itemList.firstChild) {
+    itemList.removeChild(itemList.firstChild);
+  }
+
+  //check again for UI change as we made a change in the list(i.e cleared all data)
+  updateUI();
 }
 
 //----------------Add Item Function-----------------------------------------------------
 function addItem(e) {
+  //preventing default behavoiur
   e.preventDefault();
 
   //validating the input
   if (itemInput.value === "") {
     alert("Please Enter an Item");
   } else {
-    createLi(itemInput.value);
-
-    updateUI();
-
+    createLiElement(itemInput.value);
     itemInput.value = "";
+
+    //check again for UI, as we made a change in the list(i.e added a data)
+    updateUI();
+  }
+}
+
+/* function to check UI after page load to make sure we need to display filter input and clear all button to make it look more dynamic */
+
+function updateUI() {
+  /* dynamically load the length of the li in real time to handle the UI state of filter input and clear button as it depends on the length of the listItem  */
+
+  const items = itemList.querySelectorAll("li");
+  if (items.length === 0) {
+    clearBtn.style.display = "none";
+    filterInput.style.display = "none";
+  } else {
+    clearBtn.style.display = "block";
+    filterInput.style.display = "block";
   }
 }
 
 //---------------------------------Event Listeners----------------------------------------
 
-form.addEventListener("submit", addItem);
-itemList.addEventListener("click", handleClick);
-filterInput.addEventListener("input", handleFilter);
+/* adding all the event listeners inside DOMContentLoaded event listener, to ensure that the DOM elements which we  are trying to interact with in our JavaScript code actually exist.  */
+document.addEventListener("DOMContentLoaded", () => {
+  form.addEventListener("submit", addItem);
+  itemList.addEventListener("click", removeItem);
+  clearBtn.addEventListener("click", clearItems);
+  updateUI();
+});
